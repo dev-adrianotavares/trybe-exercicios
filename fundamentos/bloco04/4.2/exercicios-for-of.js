2e70bc6fca575c58dadf96eb3b72dc5e42bd72f6
dev-adrianotavares <adreanotavares@gmail.com>
2022-06-22 16:20:55 -0300

    Exercicios dia 4.2

---
+++ fundamentos/bloco04/4.2/exercicios-for-of.js
@@ -0,0 +1,8 @@
+//Exercício 
+//Utilize o for/of para imprimir os elementos da lista names com o console.log():
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for(let pessoas of names) {
+  console.log(pessoas)
+}
